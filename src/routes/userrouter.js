00b19242c86b29de8fbab17c02c6dd1a2d59bece
soijeongg <010127js@gmail.com>
2024-03-09 10:30:52 +0900

    🐛 fixed User api

--- src/routes/userrouter.js
+++ src/routes/userrouter.js
@@ -2,10 +2,11 @@ import express from "express";
 import Joi from "joi";
 import authloginmiddleware from "../middlewares/authMiddleware.js";
 import authMiddleware from "../middlewares/authMiddleware.js";
-import { changeid } from "../controller/usercontroller.js";
+import { changeid, deleteU } from "../controller/usercontroller.js";
 let router = express.Router();
 
 router.put("/", authMiddleware,changeid)
+router.delete("/", authMiddleware, deleteU)
     
 
 
